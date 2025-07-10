046f4d0e232bfa32b72b3dad80b78b2e3128311d
Seol Yoon <teowhite0423@gmail.com>
2025-07-10 16:48:17 +0900

    feat: 보통문서 스레드 생성 기능 추가

--- main.py
+++ main.py
@@ -24,6 +24,9 @@ logger = logging.getLogger("main")
 # 엠베드 색 지정
 EmbedColor = {"default": 0xDADADA, "success": 0x77DADA, "error": 0xB32424}
 
+# slash command group 지정
+debate = bot.slash_command_group("토론", "토론 포스트를 생성하는 명령어입니다.")
+
 # 시작 시간 기록
 bot.start_time = time.time()
 
@@ -37,7 +40,7 @@ async def on_ready():
     # 활동 변경
     await bot.change_presence(
         status=discord.Status.online,
-        activity=discord.Game("!help"),
+        activity=discord.Game("V 0.0.0 (alpha)"),
     )
 
 
@@ -50,6 +53,25 @@ async def Ping(ctx):
     embed.add_field(name="", value="응답 완료!")
     await ctx.respond(embed=embed)
 
+
+@debate.command(name="보통문서", description=("보통문서에 관한 토론 포스트를 생성합니다.")
+async def Normal(ctx, article: discord.Option(str, name="문서명", description="토론 대상이 되는 문서 이름을 작성해주세요."), reason: discord.Option(str, name="발제이유", description="발제하는 이유를 5글자 안팎으로 간단하게 요약해서 적어주세요.")):
+    wikiGuild = bot.get_guild(1392679550730637382)
+    try:
+        guildUser = await wikiGuild.fetch_member(ctx.author.id)
+    except discord.NotFound:
+        await ctx.respond("당신은 대상 서버에 참여하고 있지 않습니다.", ephemeral=True)
+        return
+    if 1392684765068394698 in guildUser.roles.id:
+        normalForum = bot.get_channel(1392690787447214130)
+        newThread = await normalForum.create_thread(
+        name=f"{article} - {reason}",
+        content=f"발제자: <@{ctx.user.id}>"
+        )
+        await ctx.respond(f"{newThread.jump_url} 토론이 생성되었습니다!")
+        await newThread.starting_message.edit(content=f"발제자: <@{ctx.user.id}>\n문서의 최상단 분류 아래에 `[include (틀:디스코드 토론,url={newThread.jump_url})]`을 삽입해주세요.")
+    else:
+        await ctx.respond("마작위키 계정이 인증되지 않았습니다. <#1392679551825346633>에서 인증을 진행해주시고, 이미 인증 요청을 했다면 잠시 기다려주세요.", ephemeral=True)
 
 
 bot.run(os.getenv("BOT_TOKEN"))
